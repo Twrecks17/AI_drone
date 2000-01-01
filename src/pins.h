945cffa02c2dae2f41420c3f744f82474efb221a
Donovan McKelvey <programmingmind@gmail.com>
2000-01-01 00:17:37 +0000

    Initial defines for GPIO and analog pins

---
+++ src/pins.h
@@ -0,0 +1,52 @@
+#ifndef PINS_H
+#define PINS_H
+
+// GPIO pins
+#define P8_3  38
+#define P8_4  39
+#define P8_5  34
+#define P8_6  35
+#define P8_11 45
+#define P8_12 44
+#define P8_14 26
+#define P8_15 47
+#define P8_16 46
+#define P8_17 27
+#define P8_18 65
+#define P8_20 63
+#define P8_21 62
+#define P8_22 37
+#define P8_23 36
+#define P8_24 33
+#define P8_25 32
+#define P8_26 61
+#define P8_27 86
+#define P8_28 88
+#define P8_29 87
+#define P8_30 89
+#define P8_39 76
+#define P8_40 77
+#define P8_41 74
+#define P8_42 75
+#define P8_43 72
+#define P8_44 73
+#define P8_45 70
+#define P8_46 71
+
+#define P9_12 60
+#define P9_15 48
+#define P9_23 49
+#define P9_25 117
+#define P9_27 115
+#define P9_42 7
+
+// Analog input
+#define P9_33 4
+#define P9_35 6
+#define P9_36 5
+#define P9_37 2
+#define P9_38 3
+#define P9_39 0
+#define P9_40 1
+
+#endif
